38d422fad69454e60211cbf6c412713715987aad
olu2001segun <55858188+olu2001segun@users.noreply.github.com>
2020-06-05 18:32:19 +0100

    Create EA_Sports-HNG-06241.js

---
+++ scripts/EA_Sports-HNG-06241.js
@@ -0,0 +1,6 @@
+var name = "Olusegun Tayo";
+var id = "HNG-06241";
+var email = "oluseguntayo2001@gmail.com";
+var language = "JavaScript";
+
+console.log("Hello World, this is "+ name + " with HNGi7 ID " + id  + " and email " + email + " using" + " " + language + " " + "for stage 2 task");
